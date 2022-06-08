d0715582bd231946cfd5eb23212cd763394dd28b
Jordan Voss <jordan.voss2@mail.dcu.ie>
2022-06-08 21:36:17 +0100

    get video to fill width and nav bar on top

--- screens/web/Homepage.js
+++ screens/web/Homepage.js
@@ -3,6 +3,7 @@ import {View, Text, StyleSheet, ScrollView, Button, Platform} from 'react-native
 import {Link} from "@react-navigation/web";
 import Header from "../../components/Header";
 import {Video} from "expo-av";
+import { Dimensions } from 'react-native';
 
 
 const isWeb = Platform.OS === 'web';
@@ -12,6 +13,8 @@ export default class Homepage extends React.Component {
     static navigationOptions = {
         title: ""
     }
+    windowWidth = Dimensions.get('window').width;
+    windowHeight = Dimensions.get('window').height;
 
     render() {
         return (
@@ -20,8 +23,9 @@ export default class Homepage extends React.Component {
                     <View> 
             <Video
 	  source={require('../../assets/video/odyssey.mp4')}
-        style={{width: 320, height: 200}}
+        style={{width: this.windowWidth, height: 200}}
         isMuted={true}
+        isLooping={true}
         shouldPlay={true}
         repeat={true}
         resizeMode={"cover"}
@@ -29,10 +33,9 @@ export default class Homepage extends React.Component {
         ignoreSilentSwitch={"obey"}
 	    resizeMode="cover"
 	//   style={{ width, height: 300 }}
-	/>
-                </View>
+	/></View>
 
-            <View>
+            <View style={{position: 'absolute', top: 0,justifyContent: 'center', alignItems: 'center'}}>
                 <Header></Header>
             </View>
             <View style={styles.container}>
