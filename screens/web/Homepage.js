90d1add107dbd6435d3d19853e63ae2eb4b8d122
Jordan Voss <jordan.voss2@mail.dcu.ie>
2022-06-08 21:25:28 +0100

    get video to autoplay and keep muted

--- screens/web/Homepage.js
+++ screens/web/Homepage.js
@@ -6,49 +6,28 @@ import {Video} from "expo-av";
 
 
 const isWeb = Platform.OS === 'web';
-// const video = React.useRef(null);
 
 export default class Homepage extends React.Component {
-    // video = React.useRef(null);
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         status: null
-    //     }
-    //     // const video = React.useRef(null);
-    // }
     static path = "";
     static navigationOptions = {
         title: ""
     }
 
     render() {
-        // const video = React.useRef(null);
-
-        // const handleVideoRef = async (component) => {
-        //     if (component && !this.playbackObject) {
-        //       this.playbackObject = component;
-        //       await this.playbackObject.loadAsync(Asset.fromModule(require("../../assets/video/odyssey.mp4")));
-        //       await this.playbackObject.playAsync();
-        //     }
-        //   }
         return (
             <ScrollView
-                stickyHeaderIndices={[1]}
-                showsVerticalScrollIndicator={false}
                 >
                     <View> 
-                    <Video
+            <Video
 	  source={require('../../assets/video/odyssey.mp4')}
-          shouldPlay
-          style={{    width: 320,
-            height: 200,}}
-          muted={true}
-          repeat={true}
-          resizeMode={"cover"}
-          rate={1.0}
-          ignoreSilentSwitch={"obey"}
-	  resizeMode="cover"
+        style={{width: 320, height: 200}}
+        isMuted={true}
+        shouldPlay={true}
+        repeat={true}
+        resizeMode={"cover"}
+        rate={1.0}
+        ignoreSilentSwitch={"obey"}
+	    resizeMode="cover"
 	//   style={{ width, height: 300 }}
 	/>
                 </View>
