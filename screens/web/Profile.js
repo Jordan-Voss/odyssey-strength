efcb475caa581ecf38d39c4cb4b303d7251c14df
Jordan Voss <jordan.voss2@mail.dcu.ie>
2022-06-09 11:40:23 +0100

    add dimensions to profile page

--- screens/web/Profile.js
+++ screens/web/Profile.js
@@ -2,17 +2,24 @@ import React from 'react';
 import {View, Text, StyleSheet, ScrollView,Button, TouchableOpacity, Platform} from 'react-native';
 import {Link} from "@react-navigation/web";
 import Header from "../../components/Header";
+import { useWindowDimensions } from 'react-native';
+
 
 
 export default function Profile(props) {
     // static path = "profile";
+    const windowWidth = useWindowDimensions().width;
+    const windowHeight = useWindowDimensions().height;
         return(
         <ScrollView
         style={styles.scrollview}
         stickyHeaderIndices={[1]}
         showsVerticalScrollIndicator={false}
         >
-            <View>
+                        <View style={{position:'absolute',   justifyContent: 'space-evenly',
+    alignItems: 'space-evenly',
+    top: windowHeight*0.1,
+    right: windowWidth*0.08,}}>
                 <Header navigation={props.navigation}></Header>
             </View>
             <View style={styles.container}>
