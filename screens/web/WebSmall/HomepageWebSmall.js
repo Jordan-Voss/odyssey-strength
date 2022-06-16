303bdded1c5669dff8ce9f31fc7ce0520e355d32
Jordan Voss <jordan.voss2@mail.dcu.ie>
2022-06-16 09:14:46 +0100

    css for wondowwidth text

--- screens/web/WebSmall/HomepageWebSmall.js
+++ screens/web/WebSmall/HomepageWebSmall.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet, Button, Platform } from "react-native";
 import { Link } from "@react-navigation/web";
+import { useWindowDimensions } from "react-native";
 
 const isWeb = Platform.OS === "web";
 
 export default function HomepageWebSmall({ navigation }) {
+  const windowWidth = useWindowDimensions().width;
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Button
         onPress={() => navigation.openDrawer()}
         title="Go to notifications"
       />
+      <Text style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        {windowWidth}
+      </Text>
     </View>
   );
 }
