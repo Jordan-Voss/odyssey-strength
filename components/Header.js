9d8139b573224e16dcd12d2bc430cea0f7c52fb4
jordanvoss98 <jordanvoss98@gmail.com>
2022-06-29 14:25:22 +0100

    check commit publishes site

--- components/Header.js
+++ components/Header.js
@@ -56,7 +56,7 @@ export default function Header(props) {
           }
         >
           <Text style={{ fontSize: fontDimension * 15, color: "white" }}>
-            Coaching
+            Coaching2
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
